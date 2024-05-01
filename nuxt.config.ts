d9c994cc6397047f2735bc776b4ca58540b7570e
XYCode Kerman <xycode-xyc@outlook.com>
2024-05-01 15:19:13 +0800

    feat: 安装tailwindcss和daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: process.env.DEBUG == 'true' },
+  css: ['assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   build: {
     transpile: ['trpc-nuxt']
   },
